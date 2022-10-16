04a280364be992a04c82da4f8444ae09cb14296f
srichandanahr <srichandana.hr@gmail.com>
2022-10-16 16:42:25 +0200

    Reverting ft loss

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -19,24 +19,6 @@ __email__ = "chethan.radhakrishna@st.ovgu.de"
 __status__ = "Development"
 
 
-# class FocalTverskyLoss(nn.Module):
-#     def __init__(self, smooth=1, gamma=0.75, alpha=0.7):
-#         super(FocalTverskyLoss, self).__init__()
-#         self.smooth = smooth
-#         self.gamma = gamma
-#         self.alpha = alpha
-#
-#     def forward(self, y_pred, y_true):
-#         y_true_pos = torch.flatten(y_true)
-#         y_pred_pos = torch.flatten(y_pred)
-#         true_pos = torch.sum(y_true_pos * y_pred_pos)
-#         false_neg = torch.sum(y_true_pos * (1 - y_pred_pos))
-#         false_pos = torch.sum((1 - y_true_pos) * y_pred_pos)
-#         pt_1 = (true_pos + self.smooth) / (
-#                 true_pos + self.alpha * false_neg + (1 - self.alpha) * false_pos + self.smooth)
-#         # return pow((1 - pt_1), self.gamma)
-#         return pow(abs(1 - pt_1), self.gamma)
-
 class FocalTverskyLoss(nn.Module):
     def __init__(self, smooth=1, gamma=0.75, alpha=0.7):
         super(FocalTverskyLoss, self).__init__()
@@ -45,11 +27,11 @@ class FocalTverskyLoss(nn.Module):
         self.alpha = alpha
 
     def forward(self, y_pred, y_true):
-        y_true_pos = torch.flatten(y_true, 1)
-        y_pred_pos = torch.flatten(y_pred, 1)
-        true_pos = torch.sum(y_true_pos * y_pred_pos, 1)
-        false_neg = torch.sum(y_true_pos * (1 - y_pred_pos), 1)
-        false_pos = torch.sum((1 - y_true_pos) * y_pred_pos, 1)
+        y_true_pos = torch.flatten(y_true)
+        y_pred_pos = torch.flatten(y_pred)
+        true_pos = torch.sum(y_true_pos * y_pred_pos)
+        false_neg = torch.sum(y_true_pos * (1 - y_pred_pos))
+        false_pos = torch.sum((1 - y_true_pos) * y_pred_pos)
         pt_1 = (true_pos + self.smooth) / (
                 true_pos + self.alpha * false_neg + (1 - self.alpha) * false_pos + self.smooth)
         # return pow((1 - pt_1), self.gamma)
